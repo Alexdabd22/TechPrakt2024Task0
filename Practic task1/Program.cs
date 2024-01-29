ab15f0f2c06ccf5410ba57b2933987c1651cf3fa
Pastukhov Aleksandr <ipz224_pok@student.ztu.edu.ua>
2024-01-29 19:20:47 +0200

    Добавленно гіт ігнор до Консольного проекту

---
+++ Practic task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть перше число:");
+        
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+
+        Console.WriteLine($"Сума чисел {num1} і {num2} дорівнює: {sum}");
+
+        Console.ReadLine();
+    }
+}
